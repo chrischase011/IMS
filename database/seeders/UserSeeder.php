fe5180ac63d31d79d325ac8e6ca93a5490941f2e
Christopher Chase <christopher.chase@dice205.com>
2023-10-14 22:16:41 +0800

    Force https

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -42,5 +42,32 @@ class UserSeeder extends Seeder
             'phone' => '091234567890',
             'roles' => 3
         ]);
+
+        User::updateOrCreate([
+            'firstname' => 'Admin',
+            'lastname' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('admin'),
+            'phone' => '091234567890',
+            'roles' => 1
+        ]);
+
+        User::updateOrCreate([
+            'firstname' => 'Employee',
+            'lastname' => 'Employee',
+            'email' => 'employee@gmail.com',
+            'password' => Hash::make('employee'),
+            'phone' => '091234567890',
+            'roles' => 2
+        ]);
+
+        User::updateOrCreate([
+            'firstname' => 'Customer',
+            'lastname' => 'Customer',
+            'email' => 'customer@gmail.com',
+            'password' => Hash::make('customer'),
+            'phone' => '091234567890',
+            'roles' => 3
+        ]);
     }
 }
